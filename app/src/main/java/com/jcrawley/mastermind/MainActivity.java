982f2be560126c98aa3507ba533b74ebc26678bb
john <johncrawley1@gmail.com>
2025-03-11 23:59:26 +0000

    Further setup of game over panel. Also current row is now highlighted, and improved button layout.

--- app/src/main/java/com/jcrawley/mastermind/MainActivity.java
+++ app/src/main/java/com/jcrawley/mastermind/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jcrawley.mastermind;
 
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     PegColor[] possibleColors = PegColor.values();
     List<PegColor> solution = new ArrayList<>(pegsPerRow);
     ViewGroup gameLayout;
+    private ViewGroup gameOverPanel;
+    private TextView gameOverTitleText, gameOverMessageText;
 
 
     @Override
@@ -36,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupLayout();
         gameLayout = findViewById(R.id.gameGridLayout);
-        setupRandomAnswer();
         setupButtons();
+        setupGameOverScreen();
+        resetGame();
     }
 
 
@@ -50,8 +56,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void startGame(){
+    private void setupGameOverScreen(){
+        gameOverPanel = findViewById(R.id.gameOverPanelInclude);
+        gameOverTitleText = findViewById(R.id.gameOverTitleText);
+        gameOverMessageText = findViewById(R.id.gameOverMessageText);
 
+        gameOverPanel.setOnClickListener(v -> {
+            resetGame();
+            gameOverPanel.setVisibility(View.GONE);
+        });
     }
 
 
@@ -65,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
         setupButton(R.id.brownButton, PegColor.BROWN);
         setupButton(R.id.pinkButton, PegColor.PINK);
     }
-
 
 
     private void setupButton(int buttonId, PegColor pegColor){
@@ -85,12 +97,12 @@ public class MainActivity extends AppCompatActivity {
         if(pegsPlaced >= MAX_PEGS){
             showBadGameOver();
         }
+        log("pegs placed: " + pegsPlaced + " max pegs: " + MAX_PEGS);
     }
 
 
     private void updateViewWith(List<Clue> clues){
-        ViewGroup cluesTopLayout = (ViewGroup) getRowForCurrentIndex().
-                getChildAt(pegsPerRow);
+        ViewGroup cluesTopLayout = (ViewGroup) getRow(currentRow).getChildAt(pegsPerRow);
         ViewGroup cluesLayout = (ViewGroup) cluesTopLayout.getChildAt(0);
         ViewGroup row1 = (ViewGroup) cluesLayout.getChildAt(0);
         ViewGroup row2 = (ViewGroup) cluesLayout.getChildAt(1);
@@ -124,12 +136,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void showBadGameOver(){
-        resetGame();
+        gameOverMessageText.setText(R.string.game_over_message_fail);
+        gameOverTitleText.setText(R.string.game_over_title);
+        showGameOverPanel();
     }
 
 
     private void showGoodGameOver(){
-        resetGame();
+        gameOverTitleText.setText(R.string.game_over_title_success);
+        showSuccessMessage();
+        showGameOverPanel();
+    }
+
+
+    private void showGameOverPanel(){
+        gameOverPanel.setVisibility(VISIBLE);
+    }
+
+
+    private void showSuccessMessage(){
+        if(numberOfTries == 1){
+            gameOverMessageText.setText(R.string.number_of_tries_one);
+        }
+        else{
+            var msg = getString(R.string.number_of_tries, numberOfTries);
+            gameOverMessageText.setText(msg);
+        }
     }
 
 
@@ -143,32 +175,56 @@ public class MainActivity extends AppCompatActivity {
         currentAnswer.add(pegColor);
         if(++currentIndex >= pegsPerRow){
             checkCurrentAnswer();
-            currentIndex = 0;
-            numberOfTries++;
-            currentRow++;
-            currentAnswer.clear();
+            moveToNextRow();
         }
         pegsPlaced++;
     }
 
 
+    private void moveToNextRow(){
+        currentIndex = 0;
+        numberOfTries++;
+        currentRow++;
+        currentAnswer.clear();
+        highlightCurrentRowBackground();
+    }
+
+
     private void resetGame(){
+        resetAllRowBackgrounds();
         numberOfTries = 0;
         currentRow = 0;
         currentIndex = 0;
         pegsPlaced = 0;
         currentAnswer.clear();
         setupRandomAnswer();
+        highlightCurrentRowBackground();
+    }
+
+
+    private void resetAllRowBackgrounds(){
+        int defaultBackgroundColor = getColor(R.color.pane_background);
+        for(int i = 0; i < numberOfRows; i++){
+            getRow(i).setBackgroundColor(defaultBackgroundColor);
+        }
+    }
+
+
+    private void highlightCurrentRowBackground(){
+        int highlightedBackgroundColor = getColor(R.color.highlighted_row_background);
+        if(currentRow < numberOfRows){
+            getRow(currentRow).setBackgroundColor(highlightedBackgroundColor);
+        }
     }
 
 
     public View getViewForCurrentIndex(){
-        return getRowForCurrentIndex().getChildAt(currentIndex);
+        return getRow(currentRow).getChildAt(currentIndex);
     }
 
 
-    private ViewGroup getRowForCurrentIndex(){
-        int rowIndex = numberOfRows - currentRow -1;
+    private ViewGroup getRow(int index){
+        int rowIndex = numberOfRows - index -1;
         return (ViewGroup) gameLayout.getChildAt(rowIndex);
     }
 
@@ -176,6 +232,7 @@ public class MainActivity extends AppCompatActivity {
     private void log(String msg){
         System.out.println("^^^ MainActivity: " + msg);
     }
+
 
     private int getBackgroundColorForPeg(PegColor pegColor){
         log("getBackgroundColorForPeg() color: " + pegColor.name());
@@ -199,6 +256,19 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < pegsPerRow; i++ ){
             solution.add(getRandomPegColor());
         }
+        setupSolutionPegs(solution);
+    }
+
+
+    private void setupSolutionPegs(List<PegColor> solution){
+        ViewGroup solutionPegsLayout = findViewById(R.id.solutionPegsLayout);
+        for(int i = 0 ; i < solution.size(); i++){
+            if(i >= solutionPegsLayout.getChildCount()){
+                return;
+            }
+            solutionPegsLayout.getChildAt(i)
+                    .setBackgroundColor(getBackgroundColorForPeg(solution.get(i)));
+        }
     }
 
 
@@ -206,7 +276,5 @@ public class MainActivity extends AppCompatActivity {
         int randomIndex = random.nextInt(possibleColors.length);
         return possibleColors[randomIndex];
     }
-
-
 
 }
